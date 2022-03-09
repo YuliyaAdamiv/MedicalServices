9a9de214b0a8c182525094cbac37c7bf59692af7
Yuliya <yulua03071992@gmail.com>
2022-03-09 17:05:57 +0200

    add component Clients

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import Home from './components/Home/Home';
 import Appointments from './components/Appointments/Appointments';
 import Staff from './components/Staff/Staff';
+import Clients from './components/Clients/Clients';
 
 import './App.scss';
 
@@ -13,6 +14,7 @@ class App extends Component {
         <Home />
         <Appointments />
         <Staff />
+        <Clients />
       </div>
     );
   }
