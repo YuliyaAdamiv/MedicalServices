dd91d3f9270b1504a59c86e9f36b02c99215e96a
Yuliya <yulua03071992@gmail.com>
2022-03-09 14:14:07 +0200

    add component appointments and table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import Appointments from './components/Appointments/Appointments';
 
 import './App.scss';
 
@@ -8,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home/>
+        <Home />
+        <Appointments />
       </div>
     );
   }
